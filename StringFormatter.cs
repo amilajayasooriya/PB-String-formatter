b035c119d134b4e02fbc4481384ad6913c69725a
amilajayasooriya <amilajayasooriya@gmail.com>
2026-02-28 01:05:56 +1100

    Amila refactoring on ToCommaSeparatedList method

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -16,19 +16,26 @@ namespace PointsBet_Backend_Online_Code_Test
     {
 
         //Code to improve
-        public static string ToCommaSepatatedList(string[] items, string quote)
-        {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
+        public static string ToCommaSeparatedList(string[] items, string delimiter)
+		{
+			StringBuilder qry = new StringBuilder();
 
-            if (items.Length > 1)
-            {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
-            }
-
-            return qry.ToString();
-        }
+			if(items != null)
+			{
+				for(int i = 0; i < items.Length; i++)
+				{
+					string line;
+					if (i == items.Length - 1)
+					{
+						line = string.Format("{0}", items[i]);
+					} else
+					{
+						line = string.Format("{0}{1}", items[i], delimiter);
+					}
+					qry.Append(line);
+				}
+			}
+			return qry.ToString();
+		}
     }
 }
